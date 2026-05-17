2f583bb7f4dda595720cfccdad4a4df4fcbce135
Viswanathan <m.vishwanthan06@gmail.com>
2026-05-17 15:16:31 +0000

    Backend Structure refactored

--- BackendApplication/BackendApplication/src/test/java/com/example/BackendApplication/BackendApplicationTests.java
+++ BackendApplication/BackendApplication/src/test/java/com/example/BackendApplication/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.BackendApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
