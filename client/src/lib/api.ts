2de5e26333c08420af3d357a10e868e993a70038
Viswanathan <m.vishwanthan06@gmail.com>
2026-06-21 11:35:57 +0000

    feat: add repository tracking functionality and webhook management endpoints to API client

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,3 +1,6 @@
+import { Aperture } from "lucide-react";
+import type { useFetcher } from "react-router-dom";
+
 const API_URL=import.meta.env.VITE_API_URL;
 
 export interface Repository{
@@ -38,5 +41,41 @@ export async function getGitHubRepositories():Promise<GithubRepo[]>{
 }
 
 //3. start tracking a repsoitory
-export async function trackRepository(repo:{githubRepoId: string,fullName:string})
+export async function trackRepository(repo:{githubRepoId: string;fullName:string;description:string|null; private:boolean}):Promise<Repository>{
+    const res=await fetch(`${API_URL}/api.repos`,{
+        method:"POST",
+        headers:{
+            "content-type":"application/json",
+        },
+        body:JSON.stringify(repo),
+        credentials:"include",
+    },
+    );
+
+    if(!res.ok) throw new Error("Failed to track repository");
+    return res.json();
+}
+
+//4, Enable webhook (start automated code reviews)
+
+export async function enableWebhook(repoId:number):Promise<Repository>{
+    const res=await fetch(`${API_URL}/api/repos/${repoId}/wbhook`,{
+        method:"POST",
+        credentials:"include"
+    });
+    if(!res.ok) throw new Error("Failed to enable webhook");
+    return res.json();
+}
+
+
+//5 Disable webhook(stops automated code reviews)
+export async function disableWebhook(repoId:number):Promise<Repository>{
+    const res=await fetch(`${API_URL}/api/repos/${repoId}/wbhook`,{
+        method:"DELETE",
+        credentials:"include",
+    });
+
+    if(!res.ok) throw new Error("Failed to disbale webhook");
+    return res.json();
+}
 
